02d0ba8c7548e300786aed363bf6af4a28288861
evol <evol@noreply.local>
2026-06-11 11:10:29 +0800

    移动端适配

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,  // 对外暴露服务，使得同一路由器下手机可以访问
+    port: 3071,  // 固定服务端口
+  }
 })
